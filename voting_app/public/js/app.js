1061c841410a15e716188a0e075f107f6c16e8e8
BitHero <nuhumusah874@gmail.com>
2024-04-27 07:06:53 +0000

    updating state

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,9 +1,33 @@
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      products: []
+    }
+    this.handleProductUpVote = this.handleProductUpVote.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({products: Seed.products})
+  }
   handleProductUpVote(productId) {
-    console.log(productId + 'has voted')
+    const nextProducts = this.state.products.map((product) => {
+      if(product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1
+        })
+      }
+      else {
+        return product
+      }
+    })
+    this.setState({
+      products: nextProducts
+    })
   }
 	render() {
-    const products = Seed.products.sort((a,b) => (
+    const products = this.state.products.sort((a,b) => (
       b.votes - a.votes
     ))
 
@@ -29,6 +53,11 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleUpVote = this.handleUpVote.bind(this)
+  }
   handleUpVote() {
     this.props.onVote(this.props.id)
   }
